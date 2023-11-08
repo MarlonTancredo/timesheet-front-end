d2aa40775beb7d1481d4ba2cebf11c43626cbd6b
unknown <marlon_tancredo@hotmail.com>
2023-11-08 19:22:08 +0000

    refactor: refactoring

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,7 +4,7 @@ import * as S from "./styles";
 import HomePage from "../components/home-page/HomePage";
 import SignIn from "../components/sign-in/SignIn";
 import SignUp from "../components/sign-up/SignUp";
-import Aplication from "../components/aplication/Aplication";
+import Calendar from "../components/calendar/Calendar";
 import NotFoundPage from "../components/not-found-page/NotFoundPage";
 
 import { Routes, Route, NavLink } from "react-router-dom";
@@ -17,11 +17,43 @@ const App = () => {
         setStateStateIsLogged(isLogged);
     };
 
-    const handleSignout = () => {
+    const handleSignOut = () => {
         setStateStateIsLogged(false);
     };
 
-    return !stateIsLogged ? (
+    if (!stateIsLogged) {
+        return (
+            <S.AppWrapper>
+                <S.NavWrapper>
+                    <S.LogoSection>
+                        <S.Title>Time Sheet</S.Title>
+                    </S.LogoSection>
+                    <S.PagesSection>
+                        <S.Links>
+                            <F.StyledLink>
+                                <NavLink to="/">Home</NavLink>
+                            </F.StyledLink>
+                            <F.StyledLink>
+                                <NavLink to="/sign-in">Sign in</NavLink>
+                            </F.StyledLink>
+                            <F.StyledLink>
+                                <NavLink to="/sign-up">Sign up</NavLink>
+                            </F.StyledLink>
+                        </S.Links>
+                    </S.PagesSection>
+                </S.NavWrapper>
+                <S.Content>
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/sign-in" element={<SignIn sendToApp={handleSendToApp} />}></Route>
+                        <Route path="/sign-up" element={<SignUp />} />
+                        <Route path="*" element={<NotFoundPage />} />
+                    </Routes>
+                </S.Content>
+            </S.AppWrapper>
+        );
+    }
+    return (
         <S.AppWrapper>
             <S.NavWrapper>
                 <S.LogoSection>
@@ -33,36 +65,7 @@ const App = () => {
                             <NavLink to="/">Home</NavLink>
                         </F.StyledLink>
                         <F.StyledLink>
-                            <NavLink to="/sign-in">Sign in</NavLink>
-                        </F.StyledLink>
-                        <F.StyledLink>
-                            <NavLink to="/sign-up">Sign up</NavLink>
-                        </F.StyledLink>
-                    </S.Links>
-                </S.PagesSection>
-            </S.NavWrapper>
-            <S.Content>
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/sign-in" element={<SignIn sendToApp={handleSendToApp} />}></Route>
-                    <Route path="/sign-up" element={<SignUp />} />
-                    <Route path="*" element={<NotFoundPage />} />
-                </Routes>
-            </S.Content>
-        </S.AppWrapper>
-    ) : (
-        <S.AppWrapper>
-            <S.NavWrapper>
-                <S.LogoSection>
-                    <S.Title>Time Sheet</S.Title>
-                </S.LogoSection>
-                <S.PagesSection>
-                    <S.Links>
-                        <F.StyledLink>
-                            <NavLink to="/">Home</NavLink>
-                        </F.StyledLink>
-                        <F.StyledLink>
-                            <NavLink to="/" onClick={handleSignout}>
+                            <NavLink to="/" onClick={handleSignOut}>
                                 Sign out
                             </NavLink>
                         </F.StyledLink>
@@ -71,7 +74,7 @@ const App = () => {
             </S.NavWrapper>
             <S.Content>
                 <Routes>
-                    <Route path="/" element={<Aplication />} />
+                    <Route path="/" element={<Calendar />} />
                 </Routes>
             </S.Content>
         </S.AppWrapper>
