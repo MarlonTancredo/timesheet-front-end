9423e26cd6d966b8ce8838a1ba0fc37b9614f9b9
Marlon Tancredo <marlon_tancredo@hotmail.com>
2023-11-12 15:07:42 +0000

    refactor: refactoring home-page

--- src/components/home-page/HomePage.tsx
+++ src/components/home-page/HomePage.tsx
@@ -1,16 +1,22 @@
+import { useContext } from "react";
+import { isLoggedContext } from "../../app/App";
 import * as F from "../styles/forms/styles";
 
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
-    return (
-        <F.HomeWrapper>
-            <F.HomeStyledLink>
-                <Link to={"/sign-in"}>Enter you login!</Link>
-                <Link to={"/sign-up"}>Still not registered?</Link>
-            </F.HomeStyledLink>
-        </F.HomeWrapper>
-    );
+    const { stateIsLogged } = useContext(isLoggedContext);
+    if (!stateIsLogged) {
+        return (
+            <F.HomeWrapper>
+                <F.HomeStyledLink>
+                    <Link to={"/sign-in"}>Enter you login!</Link>
+                    {/* <Link to={"/sign-up"}>Still not registered?</Link> */}
+                </F.HomeStyledLink>
+            </F.HomeWrapper>
+        );
+    }
+    return <h1> Welcome! </h1>;
 };
 
 export default HomePage;
