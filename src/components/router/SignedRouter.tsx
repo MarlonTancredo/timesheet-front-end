8ef1aea4c649ef28d033fce2f0310775395e9f8c
Marlon Tancredo <marlon_tancredo@hotmail.com>
2023-11-12 15:10:22 +0000

    refactor: refactoring signedRouter

--- src/components/router/SignedRouter.tsx
+++ src/components/router/SignedRouter.tsx
@@ -1,8 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../home-page/HomePage";
 import Calendar from "../calendar/Calendar";
-import SignIn from "../sign-in/SignIn";
-import SignUp from "../sign-up/SignUp";
 import NotFoundPage from "../not-found-page/NotFoundPage";
 
 const SignedRouter = () => {
@@ -10,8 +8,6 @@ const SignedRouter = () => {
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/calendar" element={<Calendar />} />
-            <Route path="/sign-in" element={<SignIn />}></Route>
-            <Route path="/sign-up" element={<SignUp />} />
             <Route path="*" element={<NotFoundPage />} />
         </Routes>
     );
